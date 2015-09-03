1080bf56ae68a3004f7644124f13255175f949ee
Vic <a7lexis@gmail.com>
2015-09-03 14:47:35 -0700

    Anagrams Phase 2 complete

--- anagrams.rb
+++ anagrams.rb
@@ -1,6 +1,6 @@
 require 'byebug'
 
-#Phase 1
+#Phase 1: O(n!)
 def anagram_1?(str1, str2)
   first_anagram(str1).include?(str2)
 end
@@ -9,7 +9,6 @@ def first_anagram(str)
   return [str] if str.length < 2
   anagrams = []
   str.split("").each.with_index do |char, i|
-    # debugger
     arr_of_chars = str.chars
     arr_of_chars.delete_at(i)
     str_without_char = arr_of_chars.join
@@ -18,4 +17,17 @@ def first_anagram(str)
     end
   end
   anagrams
+end
+
+# Phase 2: O(n)
+def anagram_2(str1, str2)
+  return false if str1.length != str2.length
+  str1_arr = str1.chars
+  str2_arr = str2.chars
+  str1.length.times do |i|
+    char = str1[i]
+    str1_arr.delete(char)
+    str2_arr.delete(char)
+  end
+  str1_arr.empty? && str2_arr.empty?
 end
