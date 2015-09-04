24d2243a7b88fc517be64521cc2623562955f248
Vic <a7lexis@gmail.com>
2015-09-03 20:46:27 -0700

    Finished Min Max 2 stack queue

--- min_max_stack_queue.rb
+++ min_max_stack_queue.rb
@@ -1,22 +1,20 @@
 class MyStack
-  attr_reader :max
 
   def initialize
     @store = []
-    @max = nil
-    @min = nil
+    @max = []
+    @min = []
   end
 
   def push(el)
-    @max ||= el
-    @max = el if el > @max
-    @min ||= el
-    @min = el if el < @min
+    store_max(el)
+    store_min(el)
     @store.push(el)
   end
 
   def pop
-
+    @max.pop
+    @min.pop
     @store.pop
   end
 
@@ -27,15 +25,44 @@ class MyStack
   def size
     @store.length
   end
+
+  def max
+    @max.last
+  end
+
+  def min
+    @min.last
+  end
+
+  private
+  def store_max(el)
+    if @max.empty?
+      @max.push(el)
+    else
+      el > @max.last ? @max.push(el) : @max.push(@max.last)
+    end
+  end
+
+  def store_min(el)
+    if @min.empty?
+      @min.push(el)
+    else
+      el < @min.last ? @min.push(el) : @min.push(@min.last)
+    end
+  end
 end
 
 class Queue
   def initialize
     @input = MyStack.new
     @output = MyStack.new
+    @max = []
+    @min = []
   end
 
   def enqueue(el)
+    store_max(el)
+    store_min(el)
     @input.push(el)
   end
 
@@ -43,7 +70,10 @@ class Queue
     if @output.size == 0
       @input.size.times { @output.push(@input.pop) }
     end
-    @output.pop
+    el = @output.pop
+    store_max(peek)
+    store_min(peek)
+    el
   end
 
   def peek
@@ -55,5 +85,48 @@ class Queue
 
   def size
     @input.size + @output.size
+  end
+
+  def max
+    @max
+  end
+
+  def min
+    @min
+  end
+
+  private
+  def store_max(el)
+    if el
+      in_max = @input.max
+      out_max = @output.max
+      cur_max = el
+      if in_max
+        cur_max = in_max > cur_max ? in_max : cur_max
+      end
+      if out_max
+        cur_max = out_max > cur_max ? out_max : cur_max
+      end
+      @max = cur_max
+    else
+      @max = nil
+    end
+  end
+
+  def store_min(el)
+    if el
+      in_min = @input.min
+      out_min = @output.min
+      cur_min = el
+      if in_min
+        cur_min = in_min < cur_min ? in_min : cur_min
+      end
+      if out_min
+        cur_min = out_min < cur_min ? out_min : cur_min
+      end
+      @min = cur_min
+    else
+      @min = nil
+    end
   end
 end
